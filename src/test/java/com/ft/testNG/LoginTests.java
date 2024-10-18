b19e400159af60f4e8daf32d14bb3439cecb5063
Siva <>
2024-10-18 09:48:21 +0530

    testng -1

---
+++ src/test/java/com/ft/testNG/LoginTests.java
@@ -0,0 +1,34 @@
+package com.ft.testNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+
+    @Test(priority = 1)
+    public void verifyLoginSuccess(){
+        System.out.println("Enter userName and password");
+        System.out.println("Click on Login button");
+        Assert.assertEquals(5,5);
+    }
+
+    @Test
+    public void verifyUnSuccessfulLogin(){
+        System.out.println("Enter inValid userName and valid password");
+        System.out.println("Click on Login button");
+        Assert.assertEquals("Enter valid email", "Enter valid email");
+    }
+
+    @Test(priority = 2)
+    public void verifySocialMediaLinksPresent(){
+        Assert.assertEquals(3,3);
+    }
+
+    @Test(priority = 3)
+    public void addProductsToCart(){
+        System.out.println("Add Product to cart");
+        Assert.assertEquals("Product1","Product1");
+    }
+
+}
---
+++ src/test/java/com/ft/testNG/SimpleTest.java
@@ -0,0 +1,46 @@
+package com.ft.testNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    // Automation Framework
+    // Test Automation Framework - TestNG
+
+  /*  public static void main(String[] args) {
+        System.out.println("Test");
+        new SimpleTest().test1();
+        new SimpleTest().test2();
+        new SimpleTest().test4();
+        new SimpleTest().test3();
+    }*/
+
+    @Test
+    public void webAutomation(){
+        System.out.println("Selenium");
+        Assert.assertTrue(3>2);
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void apiAutomation(){
+        System.out.println("RestAssured");
+    }
+
+    @Test
+    public void java(){
+        System.out.println("Java");
+    }
+
+    @Test
+    public void appTesting(){
+        System.out.println("Postman");
+    }
+
+    @Test
+    public void testNG(){
+        System.out.println("TestNG");
+    }
+
+}
