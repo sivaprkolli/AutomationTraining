8a0f6f08e8f90434ac31c5a7a3bc9ee6e19936ed
Siva <>
2024-12-02 09:28:47 +0530

    locators part-3

---
+++ src/test/java/com/ft/seleniumExamples/VerifyCheckBox.java
@@ -0,0 +1,34 @@
+package com.ft.seleniumExamples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class VerifyCheckBox {
+
+    public WebDriver driver;
+
+    @BeforeTest
+    public void launchBrowser() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void verifyCheckBoxIsSelected() {
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        boolean checkBoxBefore = driver.findElement(By.xpath("(//form[@id='checkboxes']/input)[1]")).isSelected();
+        System.out.println("checkBoxBefore :: " + checkBoxBefore);
+        driver.findElement(By.xpath("(//form[@id='checkboxes']/input)[1]")).click();
+        boolean checkBoxAfter = driver.findElement(By.xpath("(//form[@id='checkboxes']/input)[1]")).isSelected();
+        System.out.println("checkBoxAfter :: " + checkBoxAfter);
+    }
+
+    @AfterTest
+    public void killSession() {
+        driver.close();
+    }
+}
