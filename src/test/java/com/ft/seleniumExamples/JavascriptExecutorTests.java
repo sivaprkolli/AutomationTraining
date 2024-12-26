88d569ebcf124b22fb4d780012320c05457e85de
Siva <>
2024-12-26 09:46:29 +0530

    javascript executor -2

--- src/test/java/com/ft/seleniumExamples/JavascriptExecutorTests.java
+++ src/test/java/com/ft/seleniumExamples/JavascriptExecutorTests.java
@@ -4,12 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class JavascriptExecutorTests extends BaseSelenium {
+    JavascriptExecutor jse;
 
+    @BeforeClass
+    public void initialize(){
+        jse = (JavascriptExecutor) driver;
+    }
 
     @Test
     public void clickUsingJS() {
@@ -18,7 +24,6 @@ public class JavascriptExecutorTests extends BaseSelenium {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         WebElement acceptAllButton = driver.findElement(By.xpath("//button[text()='Accept All']"));
-
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String eleClick = "arguments[0].click()";
         ((JavascriptExecutor) driver).executeScript(eleClick, acceptAllButton);
@@ -51,12 +56,52 @@ public class JavascriptExecutorTests extends BaseSelenium {
         System.out.println(clickHereLink.getAttribute("target")); // deprecated in current version 4.27
         System.out.println(clickHereLink.getDomAttribute("target"));
         System.out.println(clickHereLink.getDomProperty("target"));
-
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String changeAttribute = "arguments[0].setAttribute('target','_self')";
         jse.executeScript(changeAttribute, clickHereLink);
         clickHereLink.click();
-
         Assert.assertEquals(driver.findElement(By.cssSelector(".example>h3")).getText(), "New Window");
+    }
+
+    @Test
+    public void scrollToElement(){
+        driver.get("https://the-internet.herokuapp.com/");
+
+        WebElement abtestLink = driver.findElement(By.cssSelector("[href*='abtest']"));
+        WebElement editorLink = driver.findElement(By.cssSelector("[href*='forgot_password']"));
+
+        // based on the pixel
+        jse.executeScript("window.scrollBy(0,200)");
+
+        // Bottom of the page
+        jse.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+
+        // scroll to the element
+        jse.executeScript("arguments[0].scrollIntoView(true)", abtestLink);
+    }
+
+    @Test
+    public void typeValueUsingJsId(){
+        //document.getElementById("user-name")
+        driver.get("https://www.saucedemo.com/");
+        jse.executeScript("document.getElementById(\"user-name\").value='Selenium'");
+        jse.executeScript("document.getElementById(\"password\").value='Selenium'");
+        jse.executeScript("document.getElementById('login-button').click()");
+    }
+
+    @Test
+    public void typeValueUsingQuerySelector(){
+        driver.get("https://www.saucedemo.com/");
+        jse.executeScript("document.querySelector('[name=\"user-name\"]').value=''");
+        jse.executeScript("document.querySelector('[name=\"user-name\"]').value='Selenium'");
+    }
+
+    @Test
+    public void getTestUsingJS() throws InterruptedException {
+        driver.get("https://www.saucedemo.com/");
+        String heading = jse.executeScript("return document.querySelector('.login_logo').innerText").toString();
+        System.out.println("heading :: " + heading);
+        jse.executeScript("alert('Welcome to SDET Training by Siva')");
+        System.out.println();
     }
 }
